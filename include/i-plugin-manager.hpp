b9f289dd846fe6ecbebd80052aab1d2cdbde1133
Mykhailo Didur <mikhaildidur2003@gmail.com>
2023-10-01 12:33:33 +0300

    Update and interface
    Add register functions for plugins.
    Add getters for plugins' functions.

--- include/i-plugin-manager.hpp
+++ include/i-plugin-manager.hpp
@@ -6,6 +6,7 @@
 
 MIDF_DECL_PORT(plugin_manager, 25001);
 
+// interface for managing plugins
 MIDF_DECL_FUNC_NO_ARGS(std::vector<std::string>, plugin_manager, get_plugins_list);
 MIDF_DECL_FUNC_NO_ARGS(std::vector<std::string>, plugin_manager, get_installed_plugins_list);
 
@@ -15,3 +16,25 @@ MIDF_DECL_FUNC(bool, plugin_manager, enable, std::string);
 MIDF_DECL_FUNC(bool, plugin_manager, disable, std::string);
 MIDF_DECL_FUNC(bool, plugin_manager, is_installed, std::string);
 MIDF_DECL_FUNC(bool, plugin_manager, is_enabled, std::string);
+
+// inteface for the plugins' initialization
+/*
+ * The plugin must register itself and its functions each time it starts.
+ */
+MIDF_DECL_FUNC(bool, plugin_manager, register_plugin, std::string /*name*/);
+MIDF_DECL_FUNC(bool, plugin_manager, register_function,
+    std::string /*plugin name*/,
+    std::string /*function name*/,
+    midf::function<std::string, std::string> /*the function*/,
+    std::string /*function description*/);
+
+// interface to interract with the plugins
+MIDF_DECL_FUNC(std::string, plugin_manager, get_plugin_info, std::string /*plugin name*/);
+MIDF_DECL_FUNC(std::string, plugin_manager, get_function_info,
+    std::string /*plugin name*/,
+    std::string /*function name*/);
+
+using PluginFunction = midf::function<std::string, std::string>; // fix preprocessing error
+MIDF_DECL_FUNC(PluginFunction, plugin_manager, get_function,
+    std::string /*plugin name*/,
+    std::string /*function name*/);
